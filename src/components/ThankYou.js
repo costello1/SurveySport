23ba33783e13f59aad466462be837575b3a8d4c4
costello1 <52323901+costello1@users.noreply.github.com>
2024-07-29 14:58:36 +0200

    ..

--- src/components/ThankYou.js
+++ src/components/ThankYou.js
@@ -9,7 +9,7 @@ const ThankYou = () => {
       <div className="thank-you-content">
         <h1>Grazie per aver completato il questionario!</h1>
         <p>La tua partecipazione è molto apprezzata.</p>
-        <Link to="/home">
+        <Link to="/">
           <button className="thank-you-button">Vuoi completare un altro questionario?</button>
         </Link>
       </div>
